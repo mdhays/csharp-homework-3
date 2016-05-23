49971b8e6e30e31a385d45d3391e47fde033e1d8
Matthew <mdhays1995@gmail.com>
2016-05-22 22:11:42 -0500

    Basic objectives done. Class, properties, and fields, are all implemented. Methods are named.

---
+++ Agency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Agency
+    {
+        private string agencyName;
+        private string country;
+        private int organizationSize;
+
+        public string AgencyName
+        {
+            get { return agencyName; }
+            set { agencyName = value; }
+        }
+        
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public int OrganizationSize
+        {
+            get { return organizationSize; }
+            set { organizationSize = value; }
+        }
+
+        public string GetAgencyInfo()
+        {
+            string message = "";
+
+            return message;
+        }
+    }
+}
